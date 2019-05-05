2e9820ee847ab300ebdd9dfd48beaf4608d007a1
Mehrdad Allahkarami <mal@norcom.de>
2019-05-05 23:53:53 +0200

    change to free mysql

--- src/main/java/com/metao/persoinfo/config/WebConfig.java
+++ src/main/java/com/metao/persoinfo/config/WebConfig.java
@@ -7,9 +7,11 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
