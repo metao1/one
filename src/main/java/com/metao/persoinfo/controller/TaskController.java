0a0e5095818213bdfe5c84f220a9ad6bbfda9cb6
Mehrdad Allahkarami <mal@norcom.de>
2019-05-05 16:01:25 +0200

    heroku ready

--- src/main/java/com/metao/persoinfo/controller/TaskController.java
+++ src/main/java/com/metao/persoinfo/controller/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://persoinfo.github.io", maxAge = 3600)
 @RequestMapping("/api/task")
 @Api(description = "Operations pertaining to tasks in Task Management System")
 public class TaskController {
