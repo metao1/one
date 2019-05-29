48280353563e070579ff38319d097c9e4e761d43
Mehrdad Allahkarami <mal@norcom.de>
2019-05-29 14:25:02 +0200

    remove liquibase

--- src/main/java/com/metao/persoinfo/PersoInfoApplication.java
+++ src/main/java/com/metao/persoinfo/PersoInfoApplication.java
@@ -3,11 +3,10 @@ package com.metao.persoinfo;
 import com.metao.persoinfo.properties.PersoInfoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class, PersoInfoProperties.class})
+@EnableConfigurationProperties({PersoInfoProperties.class})
 public class PersoInfoApplication {
 
   public static void main(String[] args) {
